1d7a02b42316e414483f0f4d6145183bff6a352d
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-24 10:52:21 -0400

    First Commit

---
+++ GoalPost/GoalPost/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalPost
+//
+//  Created by Nicholas Burcin on 10/24/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
