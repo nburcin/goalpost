e3d7145517f75691341428982ea7610a1a549e54
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-24 17:53:26 -0400

    Added GoalType Enum

--- GoalPost/GoalPost/Controller/GoalsVC.swift
+++ GoalPost/GoalPost/Controller/GoalsVC.swift
@@ -16,6 +16,9 @@ class GoalsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
 
     @IBAction func addGoalBtnPressed(_ sender: Any) {
@@ -24,3 +27,22 @@ class GoalsVC: UIViewController {
     
 }
 
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat salad twice a week.", type: .shortTerm, goalProgressAmount: 2)
+        return cell
+    }
+    
+    
+    
+    
+}
---
+++ GoalPost/GoalPost/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by Nicholas Burcin on 10/24/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
