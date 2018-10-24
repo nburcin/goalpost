c5ca62084f1ddce9a4118a43c858a9f49e951f08
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-24 12:21:38 -0400

    Setup tableviewcell

--- GoalPost/GoalPost/Controller/GoalsVC.swift
+++ GoalPost/GoalPost/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  GoalPost
 //
 //  Created by Nicholas Burcin on 10/24/18.
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    // Outlets
+    
+    
+    
+   
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+        print("Button Was Pressed!")
+    }
+    
 }
 
---
+++ GoalPost/GoalPost/View/GoalCell.swift
@@ -0,0 +1,25 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Nicholas Burcin on 10/24/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+}
